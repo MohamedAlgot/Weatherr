386c29467763f2c491beaa55e4f04dfc2dba1ad3
MohamedAlgot <160554523+MohamedAlgot@users.noreply.github.com>
2025-07-02 23:00:29 +0300

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -7,30 +7,9 @@ var day = today.getDate();
 var dayIndex = today.getDay();
 var monthIndex = today.getMonth();
 
-var daysEnglish = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-var monthsEnglish = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+var daysEnglish = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var monthsEnglish = ["January", "February", "March", "April", "May", "June", "July",
+                     "August", "September", "October", "November", "December"];
 
 var monthName = monthsEnglish[monthIndex];
 var dayName = daysEnglish[dayIndex];
@@ -38,19 +17,16 @@ var two_day = daysEnglish[(dayIndex + 1) % 7];
 var three_day = daysEnglish[(dayIndex + 2) % 7];
 
 function getdata(city) {
-  var request = new XMLHttpRequest();
-  request.open(
-    "GET",
-    `http://api.weatherapi.com/v1/search.json?key=7adfa5c8c1bd4a2f944160254250107&q=${city}`
-  );
-  request.send();
-
-  request.addEventListener("load", function () {
-    var recipes = JSON.parse(request.response);
-    console.log(recipes);
-    display(recipes);
-    localStorage.setItem("weather", JSON.stringify(recipes));
-  });
+  fetch(`https://api.weatherapi.com/v1/forecast.json?key=7adfa5c8c1bd4a2f944160254250107&q=${city}&days=3`)
+    .then(res => res.json())
+    .then(data => {
+      display(data);
+      localStorage.setItem("weather", JSON.stringify(data));
+    })
+    .catch(err => {
+      console.log("Error:", err);
+      Data.innerHTML = `<p class="text-white">City not found or API error.</p>`;
+    });
 }
 
 sumbit.addEventListener("click", function () {
@@ -58,50 +34,42 @@ sumbit.addEventListener("click", function () {
 });
 
 function display(data) {
-  var cartoona = "";
-  for (let i = 0; i < data.length; i++) {
-    cartoona = `
-        <div class="box col-md-4 p-5 rounded-2 ">
-          <div class="head d-flex justify-content-between rounded-1">
-            <div>${dayName}</div>
-            <div>${day} ${monthName}</div>
-          </div>
-          <h6 class="text-white location py-2">${data[i].name}</h6>
-          <h1 class="text-white degree">${data[i].lat}℃</h1>
-          <img src="./Weather/imgi_6_113.png" alt="">
-          <span class="text-info">sunny</span>
-          <div class="icons">
-            <span class="px-2"><img src="./Weather/imgi_3_icon-umberella.png" alt=""><small class="text-white">${data[i].lon}</small></span>
-            <span class="px-2"><img src="./Weather/imgi_4_icon-wind.png" alt=""></span>
-            <span class="px-2"><img src="./Weather/imgi_5_icon-compass.png" alt=""></span>
-          </div>
-        </div>
+  var cartoona = `
+    <div class="box col-md-4 p-5 rounded-2">
+      <div class="head d-flex justify-content-between rounded-1">
+        <div>${dayName}</div>
+        <div>${day} ${monthName}</div>
+      </div>
+      <h6 class="text-white location py-2">${data.location.name}</h6>
+      <h1 class="text-white degree">${data.current.temp_c}℃</h1>
+      <img src="https:${data.current.condition.icon}" alt="">
+      <span class="text-info">${data.current.condition.text}</span>
+      <div class="icons">
+        <span class="px-2"><img src="Weather/imgi_3_icon-umberella.png" alt=""><small class="text-white">${data.current.humidity}%</small></span>
+        <span class="px-2"><img src="Weather/imgi_4_icon-wind.png" alt=""><small class="text-white">${data.current.wind_kph} km/h</small></span>
+        <span class="px-2"><img src="Weather/imgi_5_icon-compass.png" alt=""><small class="text-white">${data.current.wind_dir}</small></span>
+      </div>
+    </div>
 
-        <div class="box2 col-md-4 p-5 rounded-2">
-          <div class="head d-flex justify-content-center rounded-1">
-            <div>${two_day}</div>
-          </div>
-          <div class="data d-flex align-items-center flex-md-column py-5">
-            <img src="./Weather/imgi_6_113.png" class="text-center" alt="">
-            <h1 class="text-white degree">${data[i].lat}℃</h1>
-            <span class="text-center text-white">${data[i].lon}</span>
-            <span class="text-info text-center">sunny</span>
-          </div>
-        </div>
+    <div class="box2 col-md-4 p-5 rounded-2">
+      <div class="head d-flex justify-content-center rounded-1"><div>${two_day}</div></div>
+      <div class="data d-flex align-items-center flex-md-column py-5">
+        <img src="https:${data.forecast.forecastday[1].day.condition.icon}" alt="">
+        <h1 class="text-white degree">${data.forecast.forecastday[1].day.avgtemp_c}℃</h1>
+        <span class="text-info text-center">${data.forecast.forecastday[1].day.condition.text}</span>
+      </div>
+    </div>
 
-        <div class="box3 col-md-4 p-5 rounded-2">
-          <div class="head d-flex justify-content-center rounded-1">
-            <div>${three_day}</div>
-          </div>
-          <div class="data d-flex align-items-center flex-md-column py-5">
-            <img src="./Weather/imgi_6_113.png" class="text-center" alt="">
-            <h1 class="text-white degree">${data[i].lat}℃</h1>
-            <span class="text-center text-white"><small>${data[i].lon}</small></span>
-            <span class="text-info text-center">sunny</span>
-          </div>
-        </div>
-    `;
-  }
+    <div class="box3 col-md-4 p-5 rounded-2">
+      <div class="head d-flex justify-content-center rounded-1"><div>${three_day}</div></div>
+      <div class="data d-flex align-items-center flex-md-column py-5">
+        <img src="https:${data.forecast.forecastday[2].day.condition.icon}" alt="">
+        <h1 class="text-white degree">${data.forecast.forecastday[2].day.avgtemp_c}℃</h1>
+        <span class="text-info text-center">${data.forecast.forecastday[2].day.condition.text}</span>
+      </div>
+    </div>
+  `;
+
   Data.innerHTML = cartoona;
 }
 
